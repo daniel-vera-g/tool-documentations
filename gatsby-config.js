0678bb8ceaee5c15567321c4877386dba7969315
danielVg <danielveragi@gmail.com>
2020-06-07 14:01:46 +0200

    test rm image for ci

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,14 +16,6 @@ module.exports = {
   },
   plugins: [
     {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/static/img`,
-        name: 'uploads'
-      }
-    },
-    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'content',
